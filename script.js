db26558b140c7c893665121357b78723a60a2116
4026 <4026@ABDI-SS>
2023-11-08 11:30:45 +0330

    edited by: H.Abdi - 14020817

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-const input = document.getElementById("input");
 const btn = document.getElementById("btn");
 const printing = document.getElementById("printing");
-let num_1 = input.value;
-function print(e){
-    let para = document.createElement("p");
-    let node = document.createTextNode(e);
-    para.appendChild(node);
-    printing.appendChild(para);
+//let num_1 = input.value;
+
+let para = document.createElement("p");
+let counter = 0;
+let timerID = 0;
+
+function print() {
+    if (counter > 0) {
+        let node = document.createTextNode(counter);
+        para.appendChild(node);
+        printing.appendChild(para);
+        counter--;
+    }
+    else {
+        clearInterval(timerID);
+    }
 };
+
 function bbttnn() {
-    let a = Number(num_1);
-    while (a > 0) {
-        setInterval(print(a), 1000);
-        a--;
-    }    
+    const input = document.getElementById("input");
+    counter = input.value;
+    para.innerHTML = "";
+    timerID = setInterval(print, 1000);
+
+    // let a = Number(num_1);
+    // while (a > 0) {
+    //     setInterval(print(a), 1000);
+    //     a--;
+    // }    
 }
+
 btn.addEventListener("click", bbttnn);
+
